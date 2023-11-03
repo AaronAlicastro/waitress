f945e0a7247f43581205a53bf325c60db9d26046
Aaron Alicastro <aalicastro5@gmail.com>
2023-11-03 17:47:03 -0500

    add button to change camera of the scanner

--- app/src/pages/views/components/QrScanner.jsx
+++ app/src/pages/views/components/QrScanner.jsx
@@ -1,20 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import { IconContext } from "react-icons";
+import {
+    FaFlipboard
+} from "react-icons/fa";
 import { QrReader } from "react-qr-reader";
 import { useAlert } from "react-alert";
+import BotonAcc from "./BotonAcc";
 
 function QrScanner(props) {
-  let alert = useAlert();
+    let alert = useAlert();
+    let [typeOfCamera, SetTypeOfCamera] = useState("user");
 
-    return <QrReader
-        onResult={(result, e) => {
-            if (e.name != "NotAllowedError") {
-                if (result) if (result.text) {
-                    console.log(result);
-                }
-            } else alert.show("Debes dar permiso a la cámara")
-        }}
-        constraints={props.constraints}
-    />
+    return <div>
+        <QrReader
+            onResult={(result, e) => {
+                if (e.name != "NotAllowedError") {
+                    if (result) if (result.text) {
+                        console.log(result);
+                    }
+                } else alert.show("Debes dar permiso a la cámara")
+            }}
+            constraints={{ facingMode: typeOfCamera }}
+        />
+        <div className="flexRowCenter">
+            <BotonAcc onClick={() => {
+                if (typeOfCamera == "user") SetTypeOfCamera("environment");
+                else SetTypeOfCamera("user");
+            } } >
+                <IconContext.Provider value={{ size: "0.7em" }}>
+                    <FaFlipboard />
+                </IconContext.Provider>
+            </BotonAcc>
+        </div>
+    </div>
 }
 
 export default QrScanner;
